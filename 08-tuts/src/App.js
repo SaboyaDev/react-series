21c4520382f0edcfa86126c47280855831a44cb5
Jose L Saboya <saboyadev@gmail.com>
2022-11-10 01:23:43 -0500

    changed the styling of the app

---
+++ 08-tuts/src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
+
+function App() {
+	return (
+		<div className='App'>
+			<Header title='Grocery List' />
+			<Content />
+			<Footer />
+		</div>
+	)
+}
+
+export default App
